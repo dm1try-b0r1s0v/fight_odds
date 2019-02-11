e7b9446ccf8dd5437e18b07c3477de016da9078d
sc0ff3r <kirillkor@gmail.com>
2019-02-11 13:48:39 +0300

    get fighters data

--- odds/odds/spiders/fight_odds.py
+++ odds/odds/spiders/fight_odds.py
@@ -5,7 +5,6 @@ from ..items import EventItem, FighterItem
 
 class FightOddsSpider(scrapy.Spider):
     name = 'fight_odds'
-    allowed_domains = ['https://www.bestfightodds.com']
     start_urls = ['https://www.bestfightodds.com/events/ufc-231-holloway-vs-ortega-1584/']
 
     def parse(self, response):
@@ -18,3 +17,29 @@ class FightOddsSpider(scrapy.Spider):
 
         # get event name
         event['event_name'] = response.css('div.table-header a::text').extract_first()
+
+        # get event date
+        event['event_date'] = response.css('span.table-header-date::text').extract_first()
+
+        yield event
+
+        fighter = FighterItem()
+
+        # get all fighters data from page
+        for body in response.css('table.odds-table-responsive-header tbody tr'):
+
+            # get fighters id from link in tbody
+            try:
+                fighter_link = body.css('th a::attr(href)').extract_first()
+                fighter_id = fighter_link.split('-')[-1]
+
+                # get fighter name
+                fighter_name = body.css('span.tw::text').extract_first()
+                if fighter_id != '#':
+                    fighter['fighter_id'] = int(fighter_id)
+                    fighter['fighter_name'] = fighter_name
+
+                    yield fighter
+
+            except AttributeError:
+                pass
